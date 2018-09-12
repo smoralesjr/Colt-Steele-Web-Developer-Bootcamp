884bcf25bf504dca35fdcb9566b9e3ff8283bfe3
smoralesjr <smoralesjr@gmail.com>
2018-09-11 23:18:44 -0400

    While Loop Problem Set solution

--- JavaScript Exercises/While Loop Problem Set/while.js
+++ JavaScript Exercises/While Loop Problem Set/while.js
@@ -1,1 +1,12 @@
-var x = -10;
+var counterOne = -10;
+var counterTwo = 10;
+var counterThree = 300;
+var counterFour = 5;
+
+// Print all numbers between -10 and 19
+
+// Print all even numbers between 10 and 40
+
+// Print all odd numbers between 300 and 333
+
+// PRint all numbers divisible by 5 AND 3 between 5 and 50
