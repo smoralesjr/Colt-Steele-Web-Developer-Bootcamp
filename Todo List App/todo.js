630720e1520c3b01acf37f3b83ab61a6d89dd393
smoralesjr <smoralesjr@gmail.com>
2018-09-15 16:22:27 -0400

    refacoting todo.js. Created functions to make main loop more readable.

--- Todo List App/todo.js
+++ Todo List App/todo.js
@@ -1,33 +1,61 @@
-var todos = ["Buy New Turtle"];
+//Creating empty array for todo list
+var todos = [];
 var input = prompt("What would you like to do?");
 
+//function addTodo() to add to todo list
+
+const addTodo = function() {
+  //ask for new todos
+  let newTodo = prompt("Enter new todo: ");
+  //add to todos array
+  todos.push(newTodo);
+
+}
+
+//function listTodos() to list todo items
+
 const listTodos = function() {
+  
+  if (todos.length === 0){
+    console.log("The list is empty. Nothing to list.");
+  } else{
   console.log("**********");
   for (let count = 0; count < todos.length; count++) {
 
     console.log((count + 1) + ". " + todos[count]);
 
   }
+    }
   console.log("**********");
 }
 
+//function deleteTodos() to delete from todo list
+
+const deleteTodos = function() {
+  listTodos();
+  let deleteNumber = prompt("Please enter the todo number to delete: ");
+  deleteNumber = parseInt(deleteNumber);
+  deleteNumber -= 1;
+  todos.splice(deleteNumber, 1);
+  console.log("Item " + (deleteNumber + 1) + " deleted.")
+  listTodos();
+
+}
+
+//main program loop
 while (input !== "quit") {
   if (input === "list") {
+
     listTodos();
+
   } else if (input === "new") {
-    //ask for new todos
-    let newTodo = prompt("Enter new todo: ");
-    //add to todos array
-    todos.push(newTodo);
+
+    addTodo();
 
   } else if (input === "delete") {
 
-    listTodos();
-    let deleteNumber = prompt("Please enter the todo number to delete: ");
-    deleteNumber = parseInt(deleteNumber);
-    deleteNumber -= 1;
-    todos.splice(deleteNumber, 1);
-    listTodos();
+    deleteTodos();
+
   }
 
   //start the loop over
