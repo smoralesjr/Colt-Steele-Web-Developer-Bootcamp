3a13ca309757a7223ac7be1d6f698139353a2553
smoralesjr <smoralesjr@gmail.com>
2018-09-29 17:14:40 -0400

    refactoring

--- DOM Manipulation/dom.js
+++ DOM Manipulation/dom.js
@@ -1,11 +1,16 @@
 var body = document.querySelector("body");
 var button = document.querySelector("button");
 
+// button.addEventListener("click", function() {
+//   if (body.style.background === "white"){
+//     body.style.background = "purple";  
+//   } else {
+//     body.style.background = "white";
+//   }
+  
+// });
+
 button.addEventListener("click", function() {
-  if (body.style.background === "white"){
-    body.style.background = "purple";  
-  } else {
-    body.style.background = "white";
-  }
+  body.classList.toggle("purple");
   
 });
